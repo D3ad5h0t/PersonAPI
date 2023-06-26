cd94638f700feb35345547f67e0eb56058bc3ed0
D3ad5h0t <alexgl007@mail.ru>
2023-06-27 00:16:35 +1000

    Added new api methods

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using System.Xml.Schema;
 using Microsoft.EntityFrameworkCore;
 using PersonAPI.Data;
+using PersonAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +20,47 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("api/v1/people", async (AppDbContext context) =>
+{
+    var people = await context.People.ToListAsync();
 
+    return Results.Ok(people);
+});
+
+app.MapPost("api/v1/people", async (AppDbContext context, Person person) =>
+{
+    await context.People.AddAsync(person);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"/api/v1/people/{person.Id}", person);
+});
+
+app.MapPut("api/v1/people/{id}", async (AppDbContext context, int id, Person person) =>
+{
+    var personModel = await context.People.FindAsync(id);
+
+    if (personModel == null) return Results.NotFound();
+
+    personModel.FullName = person.FullName;
+    personModel.Telephone = person.Telephone;
+    personModel.DoB = person.DoB;
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("api/v1/people/{id}", async (AppDbContext context, int id) =>
+{
+    var personModel = await context.People.FindAsync(id);
+
+    if (personModel == null) return Results.NotFound();
+
+    context.People.Remove(personModel);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(personModel);
+});
 
 app.Run();
